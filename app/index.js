15a43537b472bdc60ac646ec422f795f78144a95
arlajoshna73 <135542341+arlajoshna73@users.noreply.github.com>
2025-09-23 17:50:53 +0530

    Update index.js

--- app/index.js
+++ app/index.js
@@ -5,10 +5,8 @@ app.get('/', (req, res) => {
   res.send('Hello from Node.js running in Azure Web App! I am Joshna, this is sample for my demo !!!');
 });
 
-// Use Azure’s provided PORT or 3000 for local/testing
 const port = process.env.PORT || 3000;
 
-// Start server only if not in test environment
 if (process.env.NODE_ENV !== 'test') {
   app.listen(port, () => {
     console.log(`App running on port ${port}`);
